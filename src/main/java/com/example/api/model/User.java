5d7302fed9331620c02baba8a4df1a2c1dd218af
Maxim <stenin.dev@gmail.com>
2024-09-05 19:39:31 +0300

    Add in Model created_at and updated_at

--- src/main/java/com/example/api/model/User.java
+++ src/main/java/com/example/api/model/User.java
@@ -1,6 +1,10 @@
 package com.example.api.model;
 
+import java.util.Date;
 import java.util.Set;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,21 +18,31 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-
 @Entity
 @Data
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private Long id;
+
+    @Column(length = 45)
     private String name;
-    @Column(nullable = false, unique = true)
-    private String username;
+
     @Column(nullable = false, unique = true)
     private String email;
+    
     @Column(nullable = false)
     private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
     
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
--- src/main/java/com/example/api/repository/UserRepository.java
+++ src/main/java/com/example/api/repository/UserRepository.java
@@ -10,8 +10,5 @@ import com.example.api.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByUsernameOrEmail(String username, String email);
-    Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
