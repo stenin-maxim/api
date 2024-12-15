9209e9c85b228f1e42334d5ed01b17258f86a521
Maxim <stenin.dev@gmail.com>
2024-12-15 18:10:18 +0300

    Add update and delete in controller Ad

--- src/main/java/com/example/api/controller/AdController.java
+++ src/main/java/com/example/api/controller/AdController.java
@@ -1,31 +1,37 @@
 package com.example.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.model.Ad;
 import com.example.api.repository.AdRepository;
-import com.example.api.service.AdService;
 
 @RestController
 @RequestMapping(value = "/api/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdController {
     @Autowired
-    AdService adService;
-
-    @Autowired
     AdRepository adRepository;
 
+    @GetMapping("/ads")
+    public ResponseEntity<List<Ad>> getAllAds() {
+        List<Ad> ads = adRepository.findAll();
+        return new ResponseEntity<>(ads, HttpStatus.OK);
+    }
+
     @GetMapping("/ad/{id}")
-    public ResponseEntity<Ad> getAdById(@PathVariable int id) {
+    public ResponseEntity<Ad> getAdById(@PathVariable long id) {
         Ad ad = adRepository.findById(id)
             .orElseThrow(() -> new RuntimeException("Not found Ad with id = " + id));
         return new ResponseEntity<>(ad, HttpStatus.OK);
@@ -33,7 +39,31 @@ public class AdController {
 
     @PostMapping(value = "/create-ad", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Ad> createNewAd(@RequestBody Ad ad) {
-        adService.createAd(ad);
+        adRepository.save(ad);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/update-ad/{id}")
+    public ResponseEntity<Ad> updateAd(@RequestBody Ad newAd, @PathVariable Long id) {
+        adRepository.findById(id)
+            .map(ad -> {
+                ad.setStatus(newAd.getStatus());
+                ad.setName(newAd.getName());
+                ad.setTypeAd(newAd.getTypeAd());
+                ad.setState(newAd.getState());
+                ad.setLinkVideo(newAd.getLinkVideo());
+                ad.setDescription(newAd.getDescription());
+                ad.setPrice(newAd.getPrice());
+
+                return adRepository.save(ad);
+            });
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/delete-ad/{id}")
+    public ResponseEntity<Ad> deleteAd(@PathVariable long id) {
+        adRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
