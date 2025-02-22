5fdbb4ae962ecd82627599a650ba5aa3c12dee8f
Maxim <stenin.dev@gmail.com>
2025-02-22 20:52:44 +0300

    Add method getAllReviews

--- src/main/java/com/example/api/controller/ReviewController.java
+++ src/main/java/com/example/api/controller/ReviewController.java
@@ -1,14 +1,19 @@
 package com.example.api.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.api.dto.ReviewCreateDto;
 import com.example.api.dto.ReviewDto;
+import com.example.api.mapper.ReviewMapper;
 import com.example.api.model.Review;
 import com.example.api.service.ReviewService;
 
@@ -20,10 +25,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(value = "/api/user/review", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ReviewController {
     private final ReviewService reviewService;
+    private final ReviewMapper reviewMapper;
 
+    @GetMapping("/")
+    public List<ReviewDto> getAllReviews() {
+        List<Review> review = reviewService.getAllReviews();
+        return reviewMapper.toDto(review);
+    }
+    
     @PostMapping("/create")
-    public ResponseEntity<Review> create(@Valid @RequestBody ReviewDto reviewDto) {
-        reviewService.create(reviewDto);
+    public ResponseEntity<Review> createReview(@Valid @RequestBody ReviewCreateDto reviewDto) {
+        reviewService.createReview(reviewDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
