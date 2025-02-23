e33708425bd4a86fb04262a761ef96d780c20033
Maxim <stenin.dev@gmail.com>
2025-02-23 15:32:44 +0300

    Add method delete in review

--- src/main/java/com/example/api/controller/ReviewController.java
+++ src/main/java/com/example/api/controller/ReviewController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,9 +35,15 @@ public class ReviewController {
         return reviewMapper.toDto(review);
     }
     
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<Review> createReview(@Valid @RequestBody ReviewCreateDto reviewDto) {
         reviewService.createReview(reviewDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Review> deleteReview(@PathVariable Long id) {
+        reviewService.deleteReview(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/api/service/ReviewService.java
+++ src/main/java/com/example/api/service/ReviewService.java
@@ -40,4 +40,8 @@ public class ReviewService {
 
         reviewRepository.save(review);
     }
+
+    public void deleteReview(Long id) {
+        reviewRepository.deleteById(id);
+    }
 }
